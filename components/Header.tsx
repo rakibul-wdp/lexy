2c5a546f45895a8b5f2a8e5fc115779a1716d62b
rakibul-wdp <rakibul.wdp@gmail.com>
2023-06-13 10:42:42 +0600

    setup sideNav and decorate with styles

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,23 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "@/public/assets/images/avatar.svg";
 import Logo from "@/public/assets/images/Logo.svg";
 import Image from "next/image";
 import SideNav from "./SideNav";
 
-const Header = () => {
+const Header = ({isChecked}: any) => {
   return (
     <div className="navbar p-0 bg-secondary">
       <div className="navbar-start">
-        <div className="dropdown">
-          {/* <SideNav /> */}
-        </div>
-        <Image src={Logo} width={100} height={100} alt="logo" />
+        <label
+          htmlFor="my-drawer-2"
+          className="btn btn-circle swap swap-rotate lg:hidden ml-2"
+        >
+          <input type="checkbox" />
+          <svg
+            className="swap-off fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z" />
+          </svg>
+          <svg
+            className="swap-on fill-current"
+            xmlns="http://www.w3.org/2000/svg"
+            width="32"
+            height="32"
+            viewBox="0 0 512 512"
+          >
+            <polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49" />
+          </svg>
+        </label>
+        {!isChecked && <a className="btn btn-ghost normal-case text-xl -mt-[2.0635rem] lg:hidden">
+          <Image src={Logo} width={80} height={80} alt="lexy" />
+        </a>}
       </div>
       <div className="navbar-end">
-        <a className="btn btn-primary capitalize text-secondary">Create Doc</a>
-        <div className="avatar ml-16 pr-10">
-          <div className="w-24 rounded-full">
-            <Image src={Avatar} width={20} height={10} alt="profile picture" />
+        <a className="btn btn-primary capitalize text-secondary px-7">
+          Create Doc
+        </a>
+        <div className="avatar ml-8 md:ml-16 pr-10">
+          <div className="w-20 rounded-full">
+            <Image
+              className="p-2"
+              src={Avatar}
+              width={500}
+              height={500}
+              alt="profile picture"
+            />
           </div>
         </div>
       </div>
--- components/InnerNav.tsx
+++ components/InnerNav.tsx
@@ -1,9 +1,54 @@
-import React from 'react'
+import React from "react";
+import Logo from "@/public/assets/images/Logo.svg";
+import Image from "next/image";
+import { RiHome5Fill } from "react-icons/ri";
+import { TiDocumentText } from "react-icons/ti";
+import { FaRobot } from "react-icons/fa";
+import { MdHelp } from "react-icons/md";
+import { IoMdSettings } from "react-icons/io";
 
 const InnerNav = () => {
   return (
-    <div>InnerNav</div>
-  )
-}
+    <div className="drawer-side">
+      <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+      <ul className="menu bg-accent h-screen p-0 pb-20 flex items-center justify-between">
+        <div>
+          <li>
+            <a className="tooltip tooltip-right p-0" data-tip="lexy">
+              <Image src={Logo} width={80} height={80} alt="lexy" />
+            </a>
+          </li>
+          <li className="mt-10 mb-5 flex items-center">
+            <a className="tooltip tooltip-right p-0" data-tip="Home">
+              <RiHome5Fill className="text-3xl text-secondary" />
+            </a>
+          </li>
+          <li className="mb-5 flex items-center">
+            <a className="tooltip tooltip-right p-0" data-tip="Documents">
+              <TiDocumentText className="text-3xl text-primary" />
+            </a>
+          </li>
+          <li className="flex items-center">
+            <a className="tooltip tooltip-right p-0" data-tip="Bots">
+              <FaRobot className="text-3xl text-secondary" />
+            </a>
+          </li>
+        </div>
+        <div>
+          <li className="mb-5">
+            <a className="tooltip tooltip-right p-0" data-tip="Help">
+              <MdHelp className="text-3xl text-secondary" />
+            </a>
+          </li>
+          <li>
+            <a className="tooltip tooltip-right p-0" data-tip="Settings">
+              <IoMdSettings className="text-3xl text-secondary" />
+            </a>
+          </li>
+        </div>
+      </ul>
+    </div>
+  );
+};
 
 export default InnerNav;
